bb86dafd0e5a31cabca96a5d3b57c7adb7948756
Ice7373 <160859431+Ice7373@users.noreply.github.com>
2024-06-02 12:51:25 +0000

    snippet that generates a random string of a specified length

---
+++ snippet that generates a random string of a specified length.js
@@ -0,0 +1,14 @@
+function generateRandomString(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
+  
+  // Example usage: generate a random string of length 10
+  const randomString = generateRandomString(10);
+  console.log(randomString);
+  
